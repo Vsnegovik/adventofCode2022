3bf679d2db0bfae7fb882f9535760de09d376be1
vsnegovik <snegovik.dev@gmail.com>
2022-12-02 14:33:03 +0300

    feat: task 1 part 2

--- src/task1.ts
+++ src/task1.ts
@@ -1,6 +1,17 @@
 // https://adventofcode.com/2022/day/1
-import { map, sumBy, flow, max, split } from 'lodash/fp'
+import { map, sumBy, flow, max, split, sortBy, takeRight } from 'lodash/fp'
 
 const input = flow([split('\n\n'), map(split('\n'))])(inputs.input1)
 
-export default flow([map(sumBy(Number)), max])(input)
+const output = flow([map(sumBy(Number)), max])(input)
+
+/* PART 2 */
+
+const outputMany = flow([
+  map(sumBy(Number)),
+  sortBy((x) => x),
+  takeRight(3),
+  sumBy(Number),
+])(input)
+
+export default `${output} ${outputMany}`
