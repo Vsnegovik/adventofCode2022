015f2ff13f11c3c1c8848d8925dce7be423c8961
vsnegovik <snegovik.dev@gmail.com>
2022-12-05 20:40:13 +0300

    refactor: task 4

--- src/task4.ts
+++ src/task4.ts
@@ -1,30 +1,25 @@
 // https://adventofcode.com/2022/day/4
 
-import { compact, flatten, flow, map, size, split } from 'lodash/fp'
+import { filter, flatten, flow, map, size, split } from 'lodash/fp'
 
-const input = flow([split('\n'), map(split(',')), map(map(split('-')))])(
-  inputs.input4,
-)
-
-const output = flow([
+const input = flow([
+  split('\n'),
+  map(split(',')),
+  map(map(split('-'))),
   map(flatten),
-  map(map(Number)),
-  map(
-    ([fromA, toA, fromB, toB]) =>
-      (fromA >= fromB && toA <= toB) || (fromA <= fromB && toA >= toB),
-  ),
-  compact,
-  size,
-])(input)
+  map(map(parseInt)),
+])(inputs.input4)
+
+const fullyContains = ([fromA, toA, fromB, toB]: number[]) =>
+  (fromA >= fromB && toA <= toB) || (fromA <= fromB && toA >= toB)
+
+const output = flow([filter(fullyContains), size])(input)
 
 /* PART 2 */
 
-const output2 = flow([
-  map(flatten),
-  map(map(Number)),
-  map(([fromA, toA, fromB, toB]) => !(fromA > toB || toA < fromB)),
-  compact,
-  size,
-])(input)
+const overlapAtAll = ([fromA, toA, fromB, toB]: number[]) =>
+  !(fromA > toB || toA < fromB)
+
+const output2 = flow([filter(overlapAtAll), size])(input)
 
 export default [output, output2]
