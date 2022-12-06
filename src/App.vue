7af71ccd04b13eeaebcd358e5ef24f499b41a8b5
vsnegovik <snegovik.dev@gmail.com>
2022-12-06 20:07:25 +0300

    feat: task 5 part 1

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ import task1 from './task1'
 import task2 from './task2'
 import task3 from './task3'
 import task4 from './task4'
+import task5 from './task5'
 
-const answers = reactive([task4, task3, task2, task1])
+const answers = reactive([task5, task4, task3, task2, task1])
 </script>
 
 <template>
---
+++ src/task5.ts
@@ -0,0 +1,55 @@
+// https://adventofcode.com/2022/day/5
+
+import { zip } from 'lodash'
+import {
+  compact,
+  drop,
+  flow,
+  map,
+  replace,
+  reverse,
+  split,
+  spread,
+  take,
+  parseInt,
+  each,
+  takeRight,
+  nth,
+  concat,
+  last,
+  join,
+  dropRight,
+} from 'lodash/fp'
+
+const replaceEmpty = (row: string) => row.replaceAll(/\s{4}/g, () => ' []')
+const crates = flow([
+  split('\n'),
+  take(8),
+  reverse,
+  map(replaceEmpty),
+  map(split(' ')),
+  spread(zip),
+  map(map(replace(/\[(.?)\]/, '$1'))),
+  map(compact),
+])(inputs.input5)
+
+flow([
+  split('\n'),
+  drop(10),
+  map(replace(/(move |from |to )/g, '')),
+  map(split(' ')),
+  map(map(parseInt(10))),
+  each(([move, from, to]) => {
+    crates[to - 1] = flow([
+      nth(from - 1),
+      takeRight(move),
+      reverse,
+      concat(crates[to - 1]),
+    ])(crates)
+    crates[from - 1] = dropRight(move)(crates[from - 1])
+  }),
+])(inputs.input5)
+
+const output = flow([map(last), join('')])(crates)
+
+export default [output]
